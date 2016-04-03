0761361482b6a31ccc48ba2bcceb16b691110da6
Alenka117 <alishia@seznam.cz>
2016-04-03 14:09:45 +0200

    výchozí

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SimpleLibrary config
+ * @var array
+ */
+$cfg = [
+    // database credentials
+    "dbHost" => "localhost",
+    "dbUser" => "root",
+    "dbPass" => "",
+    "dbBase" => "simplelibrary"
+];
+
+
+
+?>
