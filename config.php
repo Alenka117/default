369366f8f5e2d27a1f5912899fb2ff0da198d6af
Alenka117 <alishia@seznam.cz>
2016-04-03 14:10:16 +0200

    výchozí jazyk

--- config.php
+++ config.php
@@ -11,6 +11,16 @@ $cfg = [
     "dbBase" => "simplelibrary"
 ];
 
-
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+switch ($lang){
+    case "cs":
+        //echo "PAGE CS";
+        include("../langs/cs.php");//include check session CS
+        break;
+    default:
+        //echo "PAGE EN - Setting Default";
+        include("../langs/en.php");
+        break;
+}
 
 ?>
