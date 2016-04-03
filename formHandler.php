625e8913a3dd945d749daf871f8efdd8952270f9
Alenka117 <alishia@seznam.cz>
2016-04-03 15:14:18 +0200

    úprava funkce autor

--- formHandler.php
+++ formHandler.php
@@ -45,3 +45,58 @@ function validate(&$postParams, &$db) {
 }
 
 echo "error occurred";
+
+
+
+
+if (isset($_POST['author']) && count($_POST['author'])) {
+    $db = getDb();
+    validate($_POST['author'], $db);
+
+    $query = getInsertQuery("authors", $_POST['author']);
+    if (mysqli_query($db, $query)) {
+        $location = $_SERVER["HTTP_REFERER"];
+        $location.= (strpos($_SERVER["HTTP_REFERER"], "success=1") === false) ? "&success=1" : "";
+        header("Location: $location");
+        exit();
+    } else {
+        echo $query.PHP_EOL;
+        echo mysqli_error($db);
+    }
+}
+
+function validate(&$postParams, &$db) {
+    foreach ($postParams as $field => &$value) {
+        $value = mysqli_real_escape_string($db, $value);
+        switch ($field) {
+            case 'id':
+            case 'firstname':
+            case 'lastname':
+                if (filter_var($value, FILTER_SANITIZE_STRING) === false) {
+                    $error = "$value musi byt string";
+                    break 2;
+                }
+                break;
+            case 'born':
+            case 'deceased':
+                if (!DateTime::createFromFormat('d/m/Y', $string)) === false) {
+                    $error = "$value musi byt date";
+                    break 2;
+                }
+                break;
+            case 'nationality':
+            case 'created_at':
+                if (filter_var($value, FILTER_SANITIZE_STRING) === false) {
+                    $error = "$value musi byt string";
+                    break 2;
+                }
+                break;
+        }
+    }
+
+    if (isset($error)) {
+        die($error);
+    }
+}
+
+echo "error occurred";
